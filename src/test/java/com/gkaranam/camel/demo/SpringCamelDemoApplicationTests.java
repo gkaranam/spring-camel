e60c4611bad386a3730fedf72a8657e43b306a7a
gowtham <gowthambabu.karanam@gmail.com>
2021-02-08 21:44:12 -0600

    First commit

---
+++ src/test/java/com/gkaranam/camel/demo/SpringCamelDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkaranam.camel.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCamelDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
